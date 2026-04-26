d8fe25c58e65b7b4da5250248254c0407d9e51c1
gm8686 <50534070+gm8686@users.noreply.github.com>
2026-04-25 23:30:41 -0700

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// Bruin Quants frontend config for form submission via FormSubmit
-
-window.BQ_CONFIG = {
-  // Email where you want to RECEIVE applications
-  FORM_EMAIL: "gmckellips86@gmail.com",
-
-  // URL to send users after successful submission (thank-you page)
-  THANK_YOU_URL: "https://gm8686.github.io/thanks.html"
-};
